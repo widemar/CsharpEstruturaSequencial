f5840baf004a47ff8e53bbad518e59970734017d
widemar <widemarinternship@gmail.com>
2022-07-03 09:06:13 -0300

    C# Exercícios

---
+++ _04_Soma/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite o valor de X: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Digite o valor de Y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+int soma = x + y;
+
+Console.WriteLine($"SOMA = {soma}");
