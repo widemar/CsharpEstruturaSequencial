526c88882dbc34aece9413e02dc82e7be9c41479
widemar <widemarinternship@gmail.com>
2022-07-03 09:19:40 -0300

    C# Exercícios

---
+++ _05_Troco/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Preço unitário do produto: ");
+double precoUnitario = double.Parse(Console.ReadLine()!, info);
+Console.Write("Quantidade comprada: ");
+int quantidade = int.Parse(Console.ReadLine()!);
+Console.Write("Dinheiro recebido: ");
+double dinheiroRecebido = double.Parse(Console.ReadLine()!, info);
+
+double troco = dinheiroRecebido - precoUnitario * quantidade;
+
+Console.WriteLine($"TROCO = {troco.ToString("F2", info)}");
---
+++ _06_Circulo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+const double pi = 3.14159;
+
+Console.Write("Digite o valor do raio do circulo: ");
+double raio = double.Parse(Console.ReadLine()!, info);
+
+double area = pi * Math.Pow(raio, 2);
+
+Console.WriteLine($"AREA = {area.ToString("F3", info)}");
